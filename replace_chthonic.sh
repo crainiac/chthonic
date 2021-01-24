ceeac983387b5449514107b1f5473433c11a99ee
Alex Crain <alex@crain.xyz>
2021-01-24 02:53:16 +0000

    Renamer script now also deletes .git and itself.

--- replace_chthonic.sh
+++ replace_chthonic.sh
@@ -1,9 +1,13 @@
-# Replace the dummy package name with the desired package name.
+# This script replaces the dummy package name with the desired package name,
+# deletes chthonic version control information, and deletes itself.
+set -e
 if [ ! -z "$1" ]
 then
     mv chthonic $1
     sed -i'.bak' -e s/chthonic/$1/g Vagrantfile setup.py
     rm Vagrantfile.bak setup.py.bak
+    rm -rf .git
+    rm replace_chthonic.sh
 else
     echo 'Usage: sh replace_chthonic.sh <your-package-name>'
 fi
